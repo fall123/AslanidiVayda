f6986c2cfbecbcbd20f9a49055b9a6cdea5253d6
fall123 <vaida98@bk.ru>
2017-07-04 15:14:37 +0600

    add four operations

--- ProjectYP/ProjectYP/Form1.Designer.cs
+++ ProjectYP/ProjectYP/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.slojenie = new System.Windows.Forms.Button();
+            this.vichitanye = new System.Windows.Forms.Button();
+            this.umnojenie = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(27, 22);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(27, 50);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 1;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(177, 36);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 2;
+            // 
+            // slojenie
+            // 
+            this.slojenie.Location = new System.Drawing.Point(27, 102);
+            this.slojenie.Name = "slojenie";
+            this.slojenie.Size = new System.Drawing.Size(75, 23);
+            this.slojenie.TabIndex = 3;
+            this.slojenie.Text = "+";
+            this.slojenie.UseVisualStyleBackColor = true;
+            this.slojenie.Click += new System.EventHandler(this.slojenie_Click_1);
+            // 
+            // vichitanye
+            // 
+            this.vichitanye.Location = new System.Drawing.Point(108, 102);
+            this.vichitanye.Name = "vichitanye";
+            this.vichitanye.Size = new System.Drawing.Size(75, 23);
+            this.vichitanye.TabIndex = 4;
+            this.vichitanye.Text = "-";
+            this.vichitanye.UseVisualStyleBackColor = true;
+            this.vichitanye.Click += new System.EventHandler(this.vichitanye_Click);
+            // 
+            // umnojenie
+            // 
+            this.umnojenie.Location = new System.Drawing.Point(27, 131);
+            this.umnojenie.Name = "umnojenie";
+            this.umnojenie.Size = new System.Drawing.Size(75, 23);
+            this.umnojenie.TabIndex = 5;
+            this.umnojenie.Text = "*";
+            this.umnojenie.UseVisualStyleBackColor = true;
+            this.umnojenie.Click += new System.EventHandler(this.umnojenie_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(108, 131);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "/";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.delenie_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(786, 483);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.umnojenie);
+            this.Controls.Add(this.vichitanye);
+            this.Controls.Add(this.slojenie);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button slojenie;
+        private System.Windows.Forms.Button vichitanye;
+        private System.Windows.Forms.Button umnojenie;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- ProjectYP/ProjectYP/Form1.cs
+++ ProjectYP/ProjectYP/Form1.cs
@@ -16,5 +16,51 @@ namespace ProjectYP
         {
             InitializeComponent();
         }
+
+        //    private void textBox1_TextChanged(object sender, EventArgs e)
+        //    {
+        //         private void button4_Click(object sender, EventArgs e)
+        //    {
+        //        double a = Convert.ToDouble(textBox1.Text);
+        //        double b = Convert.ToDouble(textBox2.Text);
+        //        double result = a - b;
+        //        textBox3.Text = Convert.ToString(result);
+        //    }
+        //}
+
+
+
+
+        private void slojenie_Click_1(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox4.Text);
+            double b = Convert.ToDouble(textBox5.Text);
+            double result = a + b;
+            textBox6.Text = Convert.ToString(result);
+        }
+
+        private void vichitanye_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox4.Text);
+            double b = Convert.ToDouble(textBox5.Text);
+            double result = a - b;
+            textBox6.Text = Convert.ToString(result);
+        }
+
+        private void umnojenie_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox4.Text);
+            double b = Convert.ToDouble(textBox5.Text);
+            double result = a * b;
+            textBox6.Text = Convert.ToString(result);
+        }
+
+        private void delenie_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox4.Text);
+            double b = Convert.ToDouble(textBox5.Text);
+            double result = a / b;
+            textBox6.Text = Convert.ToString(result);
+        }
     }
 }
