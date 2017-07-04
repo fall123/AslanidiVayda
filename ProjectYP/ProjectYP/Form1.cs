70f4bd1c739c3170fbf9b1c726ea6689b2f3bbc8
fall123 <vaida98@bk.ru>
2017-07-04 16:25:24 +0600

    Factories added

--- ProjectYP/ProjectYP/Form1.cs
+++ ProjectYP/ProjectYP/Form1.cs
@@ -22,24 +22,10 @@ namespace ProjectYP
         {
             double first = Convert.ToDouble(textBox4.Text);
             double second = Convert.ToDouble(textBox5.Text);
-            double result;
-            switch (((Button)sender).Name)
-            {
-                case "add":
-                    result = first + second;
-                    break;
-                case "sub":
-                    result = first - second;
-                    break;
-                case "mul":
-                    result = first * second;
-                    break;
-                case "div":
-                    result = first / second;
-                    break;
-                default:
-                    throw new Exception("Неизвестная операция");
-            }
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator(((Button)sender).Name);
+            double result = calculator.Calculate(first, second);
+
+            
 
             textBox6.Text = Convert.ToString(result);
         }
---
+++ ProjectYP/ProjectYP/TwoArgumentsFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectYP
+{
+    class TwoArgumentsFactory
+    {
+        public static ITwoArgumentsCalculator CreateCalculator(string name)
+        {
+            switch (name)
+            {
+                case "add":
+                    return new Add();
+                    
+                case "sub":
+                    return new Sub();
+                    
+                case "mul":
+                    return new Mul();
+                   
+                case "div":
+                    return new Div();
+                    
+                default:
+                    throw new Exception("Неизвестная операция");
+            }
+        }
+    }
+}
+
